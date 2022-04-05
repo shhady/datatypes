572d684bf20592ee43a7d6ae89bc928246d614cd
shhady <shhadyse@gmail.com>
2022-04-05 23:10:59 +0300

    datatypes

---
+++ 1.1-primative_data_types.js
@@ -0,0 +1,74 @@
+
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+
+4. What is the result of 80 + 71.2?
+
+a) 151.2
+
+
+5. What is the result of "80" + 71.2?
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+
+7. Does "100" + 30 produce a number or a string?
+
+
+b)string
+
+
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+let a = 1
+var b = 2
+const c = 3
+//declare a variable and reassign a value to it
+let num = 2;
+num = 4;
+console.log(num)
+
+
+//create a variable and after that assign a string with its value being: He's got it!
+let name = "shhady";
+let name1 = `${name} He's got it`;
+console.log(name1);
+
+
+
+// 1. create a variable and assign a value on how much a restaurant bill is.
+let restBill = 200;
+
+// 2. create a variable and assign a value on how much tax they should pay.
+ let tax = 0.17;
+// 3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+ let total = (restBill*tax + restBill);
+ console.log(total)
+// Round the number 50.6 to its nearest integer
+
+
+console.log(Math.round (50.6))
+
+//Create a variable that is undefined
+let color;
